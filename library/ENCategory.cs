d7c1691ed79fe912c2e8142b8dbc7df4b59cfbac
Daniel <drp48@alu.ua.es>
2026-03-22 01:13:14 +0100

    29526347M - Agregacion ENCategory y  modificacion proweb

---
+++ library/ENCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace library
+{
+    public class ENCategory
+    {
+        private int _id;
+        private string _name;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+
+        public ENCategory() { }
+
+        public List<ENCategory> readAll()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.readAll();
+        }
+    }
+}
