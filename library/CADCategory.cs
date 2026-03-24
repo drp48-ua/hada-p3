854f0affc64f75abeb233fa0038fb10caa5cbc29
Tu Nombre y Apellidos <tu_email@alu.ua.es>
2026-03-24 11:24:19 +0100

    Y7330592T implementación de CADCategory

---
+++ library/CADCategory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace library
+{
+    public class CADCategory
+    {
+        private string constring;
+
+        public CADCategory()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        public List<ENCategory> readAll()
+        {
+            List<ENCategory> lista = new List<ENCategory>();
+            try
+            {
+                using (SqlConnection c = new SqlConnection(constring))
+                {
+                    c.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Categories", c);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        ENCategory cat = new ENCategory();
+                        cat.Id = int.Parse(dr["id"].ToString());
+                        cat.Name = dr["name"].ToString();
+                        lista.Add(cat);
+                    }
+                }
+            }
+            
+        }
+    }
+}
