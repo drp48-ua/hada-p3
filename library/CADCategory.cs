7724619e42143560d1cc7b64760705835312addf
Daniel <drp48@alu.ua.es>
2026-03-25 14:32:08 +0100

    29526347M - Revisión library

---
+++ library/CADCategory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace library
+{
+    public class CADCategory
+    {
+        private string constring;
+
+        public CADCategory()
+        {
+            constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
+        }
+
+        public List<ENCategory> readAll()
+        {
+            List<ENCategory> lista = new List<ENCategory>();
+            try
+            {
+                using (SqlConnection c = new SqlConnection(constring))
+                {
+                    c.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Categories", c);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        ENCategory cat = new ENCategory();
+                        cat.Id = int.Parse(dr["id"].ToString());
+                        cat.Name = dr["name"].ToString();
+                        lista.Add(cat);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return lista;
+        }
+    }
+}
---
+++ library/ENProduct.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace library
+{
+    public class ENProduct
+    {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Code { get => _code; set => _code = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int Amount { get => _amount; set => _amount = value; }
+        public float Price { get => _price; set => _price = value; }
+        public int Category { get => _category; set => _category = value; }
+        public DateTime CreationDate { get => _creationDate; set => _creationDate = value; }
+
+        public ENProduct() { }
+
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Amount = amount;
+            this.Price = price;
+            this.Category = category;
+            this.CreationDate = creationDate;
+        }
+
+        public bool create()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.create(this);
+        }
+
+        public bool update()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.update(this);
+        }
+
+        public bool delete()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.delete(this);
+        }
+
+        public bool read()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.read(this);
+        }
+
+        public bool readFirst()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.readFirst(this);
+        }
+
+        public bool readNext()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.readNext(this);
+        }
+
+        public bool readPrev()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.readPrev(this);
+        }
+    }
+}
